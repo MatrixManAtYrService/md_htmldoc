861d0be6cf719eadd29ee3fb3eb1547134789f23
Matt <git@matt.rixman.org>
2017-07-19 00:12:09 -0600

    better verbosity

--- md_htmldoc.sh
+++ md_htmldoc.sh
@@ -2,20 +2,33 @@
 ORIG=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd $DIR
+
 # make way for html
-HTML_DIR=$(basename $(dirname $(pwd)))_htmldoc
+HTML_DIR=$(basename $(dirname $DIR))_htmldoc
 rm --interactive=once -rf ../${HTML_DIR}
 mkdir -p ../${HTML_DIR}
 
 # mimic parent repo's directory structure in ../$HTML_DIR
 PARENT_REPO_CACHED_FILES=$(cd .. && git ls-files)
-for i in ${PARENT_REPO_CACHED_FILES[@]} ; do mkdir -p ../$HTML_DIR/$(dirname $i); done
+
+echo copying directory structure...
+for i in ${PARENT_REPO_CACHED_FILES[@]} ; do
+
+    FROM="../$(dirname $i)"
+    TO="../$HTML_DIR/$(dirname $i)"
+    printf "    %35s" "$FROM"
+    printf "  --mkdir-->"
+    printf "    %35s\n" "$TO"
+    mkdir -p "$TO"
+done
 
 # discover docs in parent repo
 PARENT_REPO_DOCUMENTATION=$(find .. | egrep '\.md$' | egrep -v '\./md_htmldoc')
 DOC_RELEVANT=$(python $DIR/get_references.py $PARENT_REPO_DOCUMENTATION)
 
 # for each documentation-relevant file
+echo extracting docs...
 for i in ${DOC_RELEVANT[@]} ; do
 
     FROM=$i
@@ -23,14 +36,14 @@ for i in ${DOC_RELEVANT[@]} ; do
 
     if [[ $i == *.md ]] ; then
         # generate html, replaicng .md links with .html links
-        printf "%35s" $FROM
-        printf " --pandoc--> " $FROM
+        printf "    %35s" $FROM
+        printf " --pandoc--> "
         printf "%-35s\n" ${TO/\.md/.html}
         pandoc -s $FROM --filter link_filter.py -o ${TO/\.md/.html}
     else
         # copy hyperlinked files
-        printf "%35s" $FROM
-        printf " ----cp----> " $FROM
+        printf "    %35s" $FROM
+        printf " ----cp----> "
         printf "%-35s\n" $TO
         cp $FROM $TO
     fi
@@ -39,5 +52,8 @@ done
 # in the parent repo, .gitignore HTML_DIR unless it is already .gitignored
 touch ../.gitignore
 grep -q -F "${HTML_DIR}/" ../.gitignore || echo "${HTML_DIR}/" >> ../.gitignore
+
+# clean up any empty directories we may have created
+find ../$HTML_DIR -type d -empty -delete
 
 cd $ORIG
